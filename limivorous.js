a097d03cf3e3c51e5a018921477ff69594a904f4
alpern <alpern@urbanairship.com>
2014-05-06 14:37:30 -0700

    Add additional handlers for properties
    - Added transform for transforming values on assignment (i.e. for
    canonicalization)
    - Added update handler for updating internal state before observers fire

--- limivorous.js
+++ limivorous.js
@@ -39,7 +39,7 @@ var limivorous =
       /**
        * Register a new observer. Observers should be functions
        * accepting a single argument, which is an event context. Event
-       * contexts have the following properties:
+       * contexts are objects with the following properties:
        *
        *  * target: the modified data model object
        *  * property: the name of the property that was modified
@@ -50,8 +50,11 @@ var limivorous =
        *                       'change', for all property updates, or
        *                       'change:propname' for receiving updates
        *                       only to specific properties.
+       * @param observer {function} A callback function
+       * @param options {object}
        */
-      target.on = function(event, observer) {
+      target.on = function(event, observer, options_) {
+        var options = options_ || {}
         var observers = observer_registry[event]
         if (observers === undefined) {
           observer_registry[event] = observers = []
@@ -74,8 +77,10 @@ var limivorous =
       target.cancel = function(event, observer) {
         if (arguments.length == 0) {
           observer_registry = {}
+
         } else if (arguments.length == 1) {
           delete observer_registry[event]
+
         } else if (arguments.length > 1 && observer_registry[event]) {
           var index = observer_registry[event].indexOf(observer)
           if (index > -1) {
@@ -84,6 +89,8 @@ var limivorous =
         }
         return target
       }
+
+      Object.defineProperty(target, 'observers', {get: function() { return observer_registry }})
 
       return self
     }
@@ -100,12 +107,26 @@ var limivorous =
      * The target object will also gain a method on() for registering
      * observers.
      *
+     * Options:
+     *   transform: A function which will transform the new value
+     *              before storing it.
+     *   update: A function which can be used for updating internal
+     *           state before observers are notified.
+     *
      * @param {Object} target The object to define the property on.
      * @param {String} name The name of the public property.
      * @param {Object} context The privately scoped backing store for the property value.
+     * @param {Object} options An optional object with additional
+     *                         options for configuring property
+     *                         behavior.
      */
-    self.defineProperty = function(target, name, context) {
+    self.defineProperty = function(target, name, context, options_) {
+      var options = options_ || {}
+
       var setter = function(value) {
+        if (options.transform && (options.transform instanceof Function)) {
+          value = options.transform(value)
+        }
         var event = {
           target: target,
           property: name,
@@ -113,6 +134,9 @@ var limivorous =
           previous: context[name]
         }
         context[name] = value
+        if (options.update && (options.update instanceof Function)) {
+          options.update()
+        }
         context.notifyObservers(self.CHANGE_EVENT + ':' + name, event)
         context.notifyObservers(self.CHANGE_EVENT, event)
         return target
